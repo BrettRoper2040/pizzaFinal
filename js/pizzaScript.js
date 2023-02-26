7d0f99fb3e10a930f294e4944b92c4ebce480ea2
Brett Roper <brettroper22@gmail.com>
2023-02-25 17:51:27 -0800

    Header changes when entering the makeline

--- js/pizzaScript.js
+++ js/pizzaScript.js
@@ -4,6 +4,10 @@ function main()
 {
     const startOrder = document.getElementById("startOrder");
     const pizzaNumber = document.getElementById("pizzaNumber");
+    const header = document.getElementById("header");
 
-    startOrder.addEventListener("click", () => {console.log(`You ordered ${pizzaNumber.value} pizzas`)});
+    startOrder.addEventListener("click", () => {
+        header.innerText = ("Welcome to the Mountain Top Pizza Palace Digital Makeline")
+        console.log(`You ordered ${pizzaNumber.value} pizzas`)
+    });
 }
