4f4ee51644a745c1adbb82f3bf9097a3c521f855
Brett Roper <brettroper22@gmail.com>
2023-02-27 13:05:38 -0800

    Fixed price calculations, added the ability to customize the side of the pizza

--- js/pizzaScript.js
+++ js/pizzaScript.js
@@ -23,7 +23,7 @@ function Order() {
   }
 
   Pizza.prototype.Price = function () {
-    this.cost += this.size * 3;
+    this.cost = this.size;
 
     if(this.sauce.length > 1)
     {
@@ -31,11 +31,11 @@ function Order() {
     }
     if(this.cheese.length > 2)
     {
-        this.cost += (.25 * (this.sauce.length -2))
+        this.cost += (.25 * (this.cheese.length -2))
     }
     if(this.toppings.length > 3)
     {
-        this.cost += (1 * (this.sauce.length -3))
+        this.cost += (1 * (this.toppings.length -3))
     }
     return this.cost;
   }
@@ -55,11 +55,9 @@ function main()
     const finishPizza = document.getElementById("finish");
     const priceDisplay = document.getElementById("price");
     let currentPizza;
-
         //This should turn nodelists into arrrays according to stack overflow
         makelineElements = Array.from(makelineElements);
         startElements = Array.from(startElements);
-
     startOrder.addEventListener("click", () => {
         header.innerText = ("Welcome to the Mountain Top Pizza Palace Digital Makeline")
         console.log(`You ordered ${pizzaNumber.value} pizzas`)
@@ -67,61 +65,61 @@ function main()
         Toggler(startElements)
         //Why does running this twice make it work? Shouldnt they all already have the visible style?
         Toggler(makelineElements)
-
         for(let i = 0; i < pizzaNumber.value; i++)
         {
             let newPizza = new Pizza();
             OrderUP.addPizza(newPizza);
         }
-
-        console.log(OrderUP)
-        currentPizza = OrderUP.pizza[x];
-        console.log(currentPizza);
-    
-
+        currentPizza = OrderUP.pizza[x];    
     });
-
  //this thing is the coolest thing I've ever coded. Basically it goes through all of the elements and adds the same event listener, so I don't have to write it 60 billion times
     makelineElements.forEach((element) => {
         element.addEventListener("click", (event) => {
             ingedientsList.innerText += "" + event.target.innerText + " ";
-            console.log(event.target.classList.contains("sauces"))
             if(event.target.classList.contains("sauces"))
         {
                 currentPizza.sauce.push(event.target.innerText);
-                console.log(currentPizza);
             }
             else if(event.target.classList.contains("cheeses"))
         {
                 currentPizza.cheese.push(event.target.innerText);
-                console.log(currentPizza);
             }
             else if(event.target.classList.contains("toppings"))
             {
                     currentPizza.toppings.push(event.target.innerText);
-                    console.log(currentPizza);
                 }
 
                 else if(event.target.classList.contains("garnish"))
                 {
                         currentPizza.finish.push(event.target.innerText);
-                        console.log(currentPizza);
                     }
+                    else if(event.target.classList.contains("size"))
+                    {
+                            currentPizza.size = (parseInt(event.target.id));
+                        }
         });
       });
-
       finishPizza.addEventListener("click", () => {
-            priceDisplay.innerText = currentPizza.Price();
+        console.log(currentPizza.Price());
+        console.log(parseFloat(priceDisplay.innerText));
+        console.log((parseFloat(priceDisplay.innerText) != NaN));
+          if(isNaN(parseFloat(priceDisplay.innerText)))
+          {
+            priceDisplay.innerText = (currentPizza.Price());
+          }
+          else
+          {
+            priceDisplay.innerText = parseFloat(priceDisplay.innerText) + (currentPizza.Price());
+          }
             x = x+1
             ingedientsList.innerText = "";
+            console.log(OrderUP)
             if(x < OrderUP.pizzaNumber)
             {
             currentPizza = OrderUP[x]
-            }
-            
+            }   
       });
 }
-
 function Toggler(element) {
     for (let i = 0; i < element.length; i++) {
     if (element[i].style.visibility === "visible") {
